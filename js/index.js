0b08544f4f9772a64c9a0f2bf1bc28255b4521d3
ryankayne <coope1rk@gmail.com>
2019-10-29 17:00:27 -0400

    setup some things and did mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const navLinks = document.querySelectorAll(".nav a").forEach(element => {
+    element.addEventListener("mouseover", () => {
+        element.style.color = "red"
+    })
+})
